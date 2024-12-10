5652ececf368fb810d45329f94b500cfc2da055b
Morgan <nikitakut3000@gmail.com>
2024-12-10 17:05:38 +0300

    Updates

--- main.cpp
+++ main.cpp
@@ -7,11 +7,27 @@
 #include <unordered_map>
 
 struct Field {
+private:
 	std::string name;
 	std::string type;
-	bool is_primary;
-	Field(std::string name, std::string type, bool is_primary)
-		: name(name), type(type), is_primary(is_primary) {}
+	std::string value;
+public:
+	Field(std::string name, std::string type)
+		: name(name), type(type){}
+
+	void setValue(const std::string& value) {
+		this->value = value;
+	}
+
+	std::string getName() const {
+		return name;
+	}
+	std::string getType() const {
+		return type;
+	}
+	std::string getValue() const {
+		return value;
+	}
 };
 
 void print_info() {
@@ -24,12 +40,12 @@ void print_info() {
 	std::cout << std::endl;
 }
 
-void deleteEntry(sqlite3 *db, sqlite3_stmt *stmt, std::string& tableName) {
+void deleteEntry(sqlite3 *db, sqlite3_stmt *stmt, const std::string& tableName) {
 	int id;
 	std::cout << "Enter an id: ";
 	std::cin >> id;
 
-	std::string query = "DELETE FROM " + tableName + " WHERE id = ?;";
+	const std::string query = "DELETE FROM " + tableName + " WHERE id = ?;";
 
 	if (sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
 		std::cerr << "SQL query preparation error: " << sqlite3_errmsg(db) << std::endl;
@@ -49,29 +65,29 @@ void deleteEntry(sqlite3 *db, sqlite3_stmt *stmt, std::string& tableName) {
 	std::cout << "Entry with ID " << id << " has been successfully deleted from table \"" << tableName << "\"." << std::endl;
 }
 
-int getAll(sqlite3 *db, sqlite3_stmt* stmt, std::string& tableName) {
-	std::string sqlSelect = "SELECT * FROM " + tableName + ";";
+int getAll(sqlite3 *db, sqlite3_stmt* stmt, const std::string& tableName) {
+	const std::string sqlSelect = "SELECT * FROM " + tableName + ";";
 	if (sqlite3_prepare_v2(db, sqlSelect.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
 		std::cerr << "SQL query preparation error: " << sqlite3_errmsg(db) << std::endl;
 		return -1;
 	}
 
 	while (sqlite3_step(stmt) == SQLITE_ROW) {
-		int id = sqlite3_column_int(stmt, 0);
+		const int id = sqlite3_column_int(stmt, 0);
 		const unsigned char* name = sqlite3_column_text(stmt, 1);
-		int age = sqlite3_column_int(stmt, 2);
+		const int age = sqlite3_column_int(stmt, 2);
 
 		std::cout << "ID: " << id << "\tName: " << name << "\tAge: " << age << std::endl;
 	}
 	return 0;
 }
 
-int getOnce(sqlite3 *db, sqlite3_stmt *stmt, std::string& tableName) {
+int getOnce(sqlite3 *db, sqlite3_stmt *stmt, const std::string& tableName) {
 	int userId;
 	std::cout << "Enter user id: ";
 	std::cin >> userId;
 
-	std::string sqlSelectUser = "SELECT id, name, age FROM " + tableName + " WHERE id = ?;";
+	const std::string sqlSelectUser = "SELECT * FROM " + tableName + " WHERE id = ?;";
 
 	if (sqlite3_prepare_v2(db, sqlSelectUser.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
 		std::cerr << "SQL query preparation error: " << sqlite3_errmsg(db) << std::endl;
@@ -80,18 +96,17 @@ int getOnce(sqlite3 *db, sqlite3_stmt *stmt, std::string& tableName) {
 
 	sqlite3_bind_int(stmt, 1, userId);
 
-	if (sqlite3_step(stmt) == SQLITE_ROW) {
-		int id = sqlite3_column_int(stmt, 0);
-		const unsigned char* name = sqlite3_column_text(stmt, 1);
-		int age = sqlite3_column_int(stmt, 2);
 
-		std::cout << "The user has been found: " << std::endl << std::endl;
-		std::cout << "ID: " << id << "\tName: " << name << "\tAge: " << age << std::endl << std::endl;
+	if (sqlite3_step(stmt) == SQLITE_ROW)
+	{
+		const int columnCount = sqlite3_column_count(stmt);
+		for (int i = 0; i < columnCount; i++)
+		{
+			const char* columnName = sqlite3_column_name(stmt, i);
+			const unsigned char* columnValue = sqlite3_column_text(stmt, i);
+			std::cout << columnName << ": " << (columnValue ? reinterpret_cast<const char*>(columnValue) : "NULL") << std::endl;
+		}
 	}
-	else {
-		std::cout << "The user with ID " << userId << " was not found." << std::endl << std::endl;
-	}
-
 	return 0;
 }
 
@@ -107,15 +122,14 @@ std::vector<std::string> getTables(sqlite3 *db, sqlite3_stmt *stmt) {
 	while (sqlite3_step(stmt) == SQLITE_ROW) {
 		const unsigned char* tableName = sqlite3_column_text(stmt, 0);
 		tableNames.push_back(std::string(reinterpret_cast<const char*>(tableName)));
-		std::cout << "- " << tableName << std::endl;
 	}
 
 	return tableNames;
 }
 
-std::unordered_map<std::string, std::string> getTableFileds(sqlite3 *db, sqlite3_stmt * stmt, const std::string& tableName) {
+std::vector<Field> getTableFields(sqlite3 *db, sqlite3_stmt * stmt, const std::string& tableName) {
 	std::string query = "PRAGMA table_info(" + tableName + ");";
-	std::unordered_map<std::string, std::string> fields;
+	std::vector<Field> fields;
 
 	if (sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
 		std::cerr << "SQL query preparation error: " << sqlite3_errmsg(db) << std::endl;
@@ -123,11 +137,12 @@ std::unordered_map<std::string, std::string> getTableFileds(sqlite3 *db, sqlite3
 	}
 
 	while (sqlite3_step(stmt) == SQLITE_ROW) {
-		const unsigned char* fieldName = sqlite3_column_text(stmt, 1);
-		if (fieldName && std::string(reinterpret_cast<const char*>(fieldName)) != "id") {
-			fields[std::string(reinterpret_cast<const char*>(fieldName))];
+		std::string fieldName = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
+		std::string fieldType = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2));
+		Field field(fieldName, fieldType);
+		if (fieldName != "id") {
+			fields.push_back(field);
 		}
-		std::cout << "- " << fieldName << std::endl;
 	}
 	return fields;
 }
@@ -150,18 +165,17 @@ void deleteTable(sqlite3 *db, sqlite3_stmt *stmt, const std::string& tableName) 
 }
 
 bool createTable(sqlite3* db, sqlite3_stmt* stmt, const std::string& tableName, const std::unordered_map<std::string, std::string>& tableFields) {
-	int exit;
 	std::string query =
 		"CREATE TABLE IF NOT EXISTS " + tableName + " (id INTEGER PRIMARY KEY AUTOINCREMENT, ";
 
-	for (auto it = tableFields.begin(); it != tableFields.end(); it++) {
+	for (auto it = tableFields.begin(); it != tableFields.end(); ++it) {
 		query += it->first + " " + it->second + " NOT NULL" + (std::next(it) != tableFields.end() ? ", " : "");
-	
+
 	}
 	query += ");";
 	std::cout << query << std::endl;
 
-	exit = sqlite3_exec(db, query.c_str(), nullptr, nullptr, nullptr);
+	int exit = sqlite3_exec(db, query.c_str(), nullptr, nullptr, nullptr);
 	if (exit != SQLITE_OK) {
 		std::cout << "Error: " << sqlite3_errmsg(db) << std::endl;
 		return false;
@@ -169,41 +183,45 @@ bool createTable(sqlite3* db, sqlite3_stmt* stmt, const std::string& tableName, 
 	return true;
 }
 
-void addEntry(sqlite3* db, sqlite3_stmt* stmt, std::string& tableName) {
+void addEntry(sqlite3* db, sqlite3_stmt* stmt, const std::string& tableName) {
 	std::string name;
 	int age;
-	std::unordered_map<std::string, std::string> fields = getTableFileds(db, stmt, tableName);
-	//std::string query = "INSERT INTO " + tableName + " (name, age) VALUES (?, ?);";
+	std::vector<Field> fields = getTableFields(db, stmt, tableName);
 	std::string query = "INSERT INTO " + tableName + " (";
 
-	for (auto it = fields.begin(); it != fields.end(); it++) {
-		std::cout << it->first << ": ";
-		std::cin >> it->second;
-		query += it->first + (std::next(it) != fields.end() ? ", " : "");
+	for (auto i = 0; i < fields.size(); i++) {
+		std::cout << fields[i].getName() << ": ";
+		std::string value;
+		std::cin >> value;
+		fields[i].setValue(value);
+		query += fields[i].getName() + (i != fields.size() - 1 ? ", " : "");
 	}
 	query += ") VALUES (";
-	for (auto it = fields.begin(); it != fields.end(); it++) {
-		query += (std::next(it) != fields.end() ? "?, " : "?");
+	for (int i = 0; i < fields.size(); i++) {
+		query += (i != fields.size() - 1 ? "?, " : "?");
 	}
 	query += ");";
 	std::cout << query << std::endl;
 
 
-	/*if (sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
+	if (sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
 		std::cerr << "SQL query preparation error: " << sqlite3_errmsg(db) << std::endl;
 		return;
 	}
 
-
-	sqlite3_bind_text(stmt, 1, name.c_str(), -1, SQLITE_TRANSIENT);
-	sqlite3_bind_int(stmt, 2, age);
-
+	for (auto& field : fields) {
+		if (field.getType() == "TEXT") {
+			sqlite3_bind_text(stmt, 1, field.getValue().c_str(), -1, SQLITE_TRANSIENT);
+		} else if (field.getType() == "INTEGER") {
+			sqlite3_bind_int(stmt, 2, std::stoi(field.getValue()));
+		}
+	}
 	if (sqlite3_step(stmt) != SQLITE_DONE) {
 		std::cerr << "Request execution error: " << sqlite3_errmsg(db) << std::endl;
 	}
 	else {
 		std::cout << "The data has been successfully added!" << std::endl;
-	}*/
+	}
 
 }
 
@@ -213,7 +231,7 @@ int main() {
 	std::cout << "--------------------------------------------------------" << std::endl;
 	while (true) {
 		std::string action;
-		std::cout << "Enter an action (*-create new database, o - open database): ";
+		std::cout << "Enter an action (* - create new database, o - open database): ";
 		std::cin >> action;
 
 		sqlite3* db = nullptr;
@@ -221,31 +239,29 @@ int main() {
 		int exit;
 
 		std::string db_name;
-		std::cout << "Enter database name: ";
-		std::cin >> db_name;
+
 		if (action == "o")
 		{
+			std::cout << "Enter database name: ";
+			std::cin >> db_name;
+			std::ifstream file(db_name);
+			if (!file) {
+				std::cerr << "Failed to open database file: " << db_name << std::endl;
+				continue;
+			}
 			exit = sqlite3_open(db_name.c_str(), &db);
-
-			const char* sqlCreateTable =
-				"CREATE TABLE IF NOT EXISTS users ("
-				"id INTEGER PRIMARY KEY AUTOINCREMENT, "
-				"name TEXT NOT NULL, "
-				"age INTEGER NOT NULL);";
-
-			// Выполняем запрос
-			exit = sqlite3_exec(db, sqlCreateTable, nullptr, nullptr, nullptr);
 			if (exit != SQLITE_OK) {
 				std::cout << "Error: " << sqlite3_errmsg(db) << std::endl;
-				return -1;
 			}
 			std::cout << "Enter an action (+ - create new table, s - select a table): ";
 		}
-		if (action == "*")
+		else if (action == "*")
 		{
+			std::cout << "Enter database name: ";
+			std::cin >> db_name;
 			std::ofstream file(db_name);
 			if (file.is_open()) {
-				std::cout << "File " << db_name << " has been successfuly created." << std::endl;
+				std::cout << "File " << db_name << " has been successfully created." << std::endl;
 				file.close();
 			}
 			else {
@@ -253,21 +269,32 @@ int main() {
 			}
 			std::cout << "Enter an action (+ - create new table): ";
 		}
+		else if (action == "exit") {
+			break;
+		}
+		else {
+			std::cout << "Enter correct action!" << std::endl;
+			continue;
+		}
 
 		std::cin >> action;
 
 		std::string currentTable;
 		bool exit_from_table = false;
-		while (true) {
-
+		while (action != "exit") {
 			if (action == "s") {
-				getTables(db, stmt);
+				std::vector<std::string> tableNames = getTables(db, stmt);
+				if (tableNames.empty()) {
+					std::cout << "There are no tables in database! Create new table" << std::endl;
+					action = "+";
+					continue;
+				}
+				for (auto& tableName : tableNames) {
+					std::cout << "- " << tableName << std::endl;
+				}
 				std::cout << "Choose a table: ";
 				std::cin >> currentTable;
 				std::cout << "Switched on " << currentTable << std::endl;
-			}
-			else {
-				//std::cout << "You must create new table!" << std::endl;
 			}
 			if (action == "+") {
 				std::cout << "Enter a table name: ";
@@ -278,7 +305,7 @@ int main() {
 				std::cin >> fields_number;
 				std::unordered_map<std::string, std::string> fields;
 				for (int i = 0; i < fields_number; i++) {
-					system("cls");
+					std::system("clear");
 					std::string name;
 					std::string type;
 
@@ -290,7 +317,7 @@ int main() {
 
 					fields[name] = type;
 				}
-				system("cls");
+					std::system("clear");
 				bool result = createTable(db, stmt, currentTable, fields);
 				if (result) {
 					std::cout << "Table " << currentTable << " has been successfully created!" << std::endl;
@@ -300,7 +327,7 @@ int main() {
 			while (true) {
 				std::cout << "Enter an action (? - print info): ";
 				std::cin >> action;
-				system("cls");
+				std::system("clear");
 				if (action == "?") print_info();
 				if (action == "+") addEntry(db, stmt, currentTable);
 				if (action == "-") deleteEntry(db, stmt, currentTable);
@@ -316,6 +343,7 @@ int main() {
 					std::cin >> confirm;
 					if (confirm == 'y') {
 						deleteTable(db, stmt, currentTable);
+						break;
 					}
 					else if(confirm == 'n') {
 						break;
